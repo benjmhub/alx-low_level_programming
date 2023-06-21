e9a53f49bbc78e330058b10239883f83f242ec2e
Benjamin Muhire ICISHAHAYO <muhbenn@gmail.com>
2023-06-21 04:54:17 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,19 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char letter;
+	int i;
+	char letter;
 
-    for (i = 0; i < 10; i++)
-    {
-        letter = 'a';
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
 
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-    }
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
 }
 
