77011baf7f5731af405a6e71662029d799fb7144
Benjamin Muhire ICISHAHAYO <muhbenn@gmail.com>
2023-06-23 17:34:13 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
- * Return: 0
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: On success, returns the character written.
+ * On error, returns EOF.
  */
 
 int _putchar(char c)
 {
-        return putchar(c);
+	return (putchar(c));
 }
+
+/**
+ * main - Causes an infinite loop
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 
-        while (i < 10)
-        {
-                _putchar(i);
-                /* Commenting out the line that increments i
-                   to prevent the infinite loop */
-                /* i++; */
-        }
+	while (i < 10)
+	{
+		_putchar(i);
+		/*
+		 * Commenting out the line that increments i
+		 * to prevent the infinite loop
+		 */
+		/* i++; */
+	}
 
-        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
 }
 
