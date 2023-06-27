bf106d6ff4209472a7645651b259850d3aa98f15
Benjamin Muhire ICISHAHAYO <muhbenn@gmail.com>
2023-06-26 19:09:53 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -11,20 +11,35 @@ int main(void)
     int nb;
 
     nb = _atoi("98");
-    printf("%d\n", nb);
+    printf("%d\n", nb); // Expected output: 98
+
     nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-    printf("%d\n", nb);
+    printf("%d\n", nb); // Expected output: -402
+
+    nb = _atoi(" ------++++++-----+++++--98");
+    printf("%d\n", nb); // Expected output: -98
+
     nb = _atoi("214748364");
-    printf("%d\n", nb);
+    printf("%d\n", nb); // Expected output: 214748364
+
     nb = _atoi("0");
-    printf("%d\n", nb);
+    printf("%d\n", nb); // Expected output: 0
+
     nb = _atoi("Suite 402");
-    printf("%d\n", nb);
+    printf("%d\n", nb); // Expected output: 402
+
     nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
+    printf("%d\n", nb); // Expected output: -98
+
     nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);
+    printf("%d\n", nb); // Expected output: 402
+
+    nb = _atoi("+++++ +-+ 2242454");
+    printf("%d\n", nb); // Expected output: 2242454
+
+    nb = _atoi("-2147483648");
+    printf("%d\n", nb); // Expected output: -2147483648
+
     return (0);
 }
+
