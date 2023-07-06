a8a7aae27a24819dad43f43665a36e42d0c5203b
Benjamin Muhire ICISHAHAYO <muhbenn@gmail.com>
2023-07-05 17:05:28 -0700

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to search in.
+ * @accept: The string of accepted characters.
+ *
+ * Return: The number of bytes in the initial segment of s
+ *         which consist only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i, j, match;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		match = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				match = 1;
+				break;
+			}
+		}
+		if (match == 0)
+			break;
+	}
+
+	return (count);
+}
+
