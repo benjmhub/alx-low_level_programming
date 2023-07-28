cf4dbf418caa7dc2f69733f75132ff141e057df1
benjmhub <muhbenn@gmail.com>
2023-07-28 11:33:56 -0700

    Add Node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,34 +4,41 @@
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
- * @head: A pointer to the pointer to the head of the list.
- * @str: The string to be duplicated and added as the new node's value.
+ * @head: A pointer to the pointer of the head (first element) of the list.
+ * @str: The data (string) to be stored in the new node.
  *
  * Return: The address of the new element, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    unsigned int len = 0;
+	list_t *new_node;
+	char *dup_str;
+	size_t str_len;
 
-    while (str[len])
-        len++;
+	if (str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
+	str_len = 0;
+	while (str[str_len])
+		str_len++;
 
-    return (new_node);
+	new_node->str = dup_str;
+	new_node->len = str_len;
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
 }
 
