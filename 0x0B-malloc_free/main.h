afa557ebc7abb7575ba3d109f690b577eea90fa6
benjmhub <muhbenn@gmail.com>
2023-07-28 13:56:34 -0700

    Flaot like a butterfly

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototype for create_array */
+char *create_array(unsigned int size, char c);
+
+/* Function prototype for simple_print_buffer */
+void simple_print_buffer(char *buffer, unsigned int size);
+
+#endif /* MAIN_H */
+
